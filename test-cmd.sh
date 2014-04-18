663001137b11f453e995b93896d47ccfa001f7b8
Matt Hollingsworth <matt@deepfield.net>
2014-04-18 11:49:40 -0400

    Add other URL endpoints

--- test-cmd.sh
+++ test-cmd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 dd if=/dev/zero of=/tmp/5kb.dat bs=1024 count=5
 dd if=/dev/zero of=/tmp/20mb.dat bs=1024 count=20000
-curl --data-binary @/tmp/5kb.dat -X POST 'http://requestb.in/19ayyx61'
-curl --data-binary @/tmp/20mb.dat -X POST 'http://requestb.in/19ayyx61'
+URL='http://requestb.in/19ayyx61'
+#URL='http://10.42.42.42:8000' # VM IP address
+#URL='10.1.10.24:8000' # Mac IP address
+curl --data-binary @/tmp/5kb.dat -X POST $URL
+curl --data-binary @/tmp/20mb.dat -X POST $URL
